f0aff4c1de216eae0fae2a040ae8ef1c2870ae45
brenno <brennorebeiro@gmail.com>
2024-05-21 21:35:08 -0300

    Change Method name

--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -5,14 +5,14 @@ import org.jetbrains.annotations.NotNull;
 public class MenuController {
 
     /**
-     * Verify if the chosen options inputed from the user has string;
-     * verifica se o que o que o usuário digitou tem letra
+     * Verify if the chosen options inputed from the user is not a digit;
+     * verifica se o que o que o usuário digitou não é digito
      * @param optionInputed String of the menu option choosed; Texto da opcao selecionada
      * @return boolean
      * **/
-    public static boolean hasLetter(@NotNull String optionInputed){
+    public static boolean isNotDigit(@NotNull String optionInputed){
         for (char c : optionInputed.toCharArray()) {
-            if (Character.isLetter(c) || !Character.isDigit(c)) {
+            if (!Character.isDigit(c)) {
                 return true;
             }
         }
@@ -27,7 +27,7 @@ public class MenuController {
     public static boolean isOptionValid(String answer){
         if (answer.isEmpty()){ // if user haven't inputed something; Se o usuário não digitou nada
             return false;
-        }else if(MenuController.hasLetter(answer)){ // if the inputed option has letter; Se a resposta tem letra
+        }else if(MenuController.isNotDigit(answer)){ // if the inputed option has letter; Se a resposta tem letra
             return false;
         }else if(Integer.parseInt(answer) > 5 || Integer.parseInt(answer) <= 0) {
             // If the option doesn't exist; Se a opção não existe
