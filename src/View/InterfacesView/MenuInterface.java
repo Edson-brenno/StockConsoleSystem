5618fe683c1d0936d9637d8f53d3b7f24da4f334
brenno <brennorebeiro@gmail.com>
2024-05-25 22:23:32 -0300

    Add interface

---
+++ src/View/InterfacesView/MenuInterface.java
@@ -0,0 +1,7 @@
+package View.InterfacesView;
+
+public interface MenuInterface {
+
+    void presentation(); // Presentation of the menu; Apresentacao do menu
+    void runMenu(); // Method That'll run everything that's needed; Método que vai rodar todos os passos necessarios
+}
